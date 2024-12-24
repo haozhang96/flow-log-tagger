5a0da59d67ebbc49cd5c49064632a69c533760ed
Hao Zhang <haozhang@gmx.com>
2024-12-24 01:35:58 -0500

    Add unit tests

---
+++ src/TableMapTest.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+class TableMapTest extends BaseUnitTest {
+    private static final long ROWS = ThreadLocalRandom.current().nextLong(1000L);
+    private static final Supplier<?> COLUMN_GENERATOR = () -> ThreadLocalRandom.current().nextDouble();
+    private static final TableMap<String, String> TARGET = new Target();
+
+    //==================================================================================================================
+    // Bootstrap
+    //==================================================================================================================
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    //==================================================================================================================
+    // Tests
+    //==================================================================================================================
+
+    @Test
+    void shouldBuildMappingsCorrectly() {
+        assert$(TARGET.size() == ROWS, "Unexpected mapping count: expected=%d, given=%d".formatted(ROWS, TARGET.size()));
+        TARGET.row(COLUMN_GENERATOR.get(), COLUMN_GENERATOR.get()); // Add a new unique mapping.
+        assert$(TARGET.size() == ROWS + 1L, "Unexpected mapping count: expected=%d, given=%d".formatted(ROWS + 1L, TARGET.size()));
+    }
+
+    //==================================================================================================================
+    // Assertion Helpers
+    //==================================================================================================================
+
+    static final class Target extends TableMap<String, String> {
+        private Target() {
+            super(
+                new TableGenerator(ROWS, COLUMN_GENERATOR, COLUMN_GENERATOR), // Every key (column 0) should be unique
+                Collectors.toMap(columns -> columns[0], columns -> columns[1])
+            );
+        }
+    }
+}
