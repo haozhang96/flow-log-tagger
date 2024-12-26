2e0a6bb14ba4a80f70ea59eb2d9f2920f11d5f35
Hao Zhang <haozhang@gmx.com>
2024-12-26 00:29:32 -0500

    Enhance FlowLogProcessorTest with strict output expectations

--- src/FlowLogProcessorTest.java
+++ src/FlowLogProcessorTest.java
@@ -1,12 +1,40 @@
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 class FlowLogProcessorTest extends BaseUnitTest {
-    private static final long ROWS = ThreadLocalRandom.current().nextLong(1000L);
-    private static final AtomicLong ROW_COUNTER = new AtomicLong();
-    private static final FlowLogProcessor TARGET =
-        new FlowLogProcessor(new FlowLogGenerator(ROWS), FlowLogProcessorTest::assertRows);
+    private static final TableSupplier DATA =
+        () -> Stream.of(
+            "2 123456789012 eni-5f6g7h8i 10.0.2.103 52.26.198.183 56000 23 6 15 7500 1620140661 1620140721 REJECT OK".split(" "),
+            "2 123456789012 eni-9k10l11m 192.168.1.5 51.15.99.115 49321 25 6 20 10000 1620140661 1620140721 ACCEPT OK".split(" "),
+            "2 123456789012 eni-1a2b3c4d 192.168.1.6 87.250.250.242 49152 110 6 5 2500 1620140661 1620140721 ACCEPT OK".split(" "),
+            "2 123456789012 eni-1a2b3c4d 10.0.1.102 172.217.7.228 1030 443 6 8 4000 1620140661 1620140721 ACCEPT OK".split(" "),
+            "2 123456789012 eni-2d2e2f3g 192.168.2.7 77.88.55.80 49153 993 6 7 3500 1620140661 1620140721 ACCEPT OK".split(" ")
+        );
+    private static final Tags TAGS =
+        new Tags(() -> Stream.of(
+            "23,tcp,sv_P1".split(","),
+            "25,tcp,sv_P1".split(","),
+            "443,tcp,sv_P2".split(",")
+        ));
+    private static final String[][] EXPECTED_OUTPUT =
+        {
+            {"Tag Counts:"},
+            {"Tag", "Count"},
+            {"Untagged", "2"},
+            {"sv_P1", "2"},
+            {"sv_P2", "1"},
+            {},
+            {"Port/Protocol Combination Counts:"},
+            {"Port", "Protocol", "Count"},
+            {"23", "tcp", "1"},
+            {"25", "tcp", "1"},
+            {"110", "tcp", "1"},
+            {"443", "tcp", "1"},
+            {"993", "tcp", "1"},
+        };
+    private static final List<String[]> OUTPUT = new ArrayList<>(EXPECTED_OUTPUT.length);
+    private static final FlowLogProcessor TARGET = new FlowLogProcessor(DATA, TAGS, rows -> rows.forEach(OUTPUT::add));
 
     //==================================================================================================================
     // Bootstrap
@@ -22,23 +50,12 @@ class FlowLogProcessorTest extends BaseUnitTest {
 
     @Test
     void run_shouldProcessFlowLogCorrectly() {
-        assert$(ROW_COUNTER.get() == 0L, "Unexpected row count before processing: " + ROW_COUNTER);
-        TARGET.run(); // Run the processor to call assertRows().
-        assert$(ROW_COUNTER.get() != 0L, "Unexpected row count after processing: " + ROW_COUNTER);
-    }
+        TARGET.run();
 
-    //==================================================================================================================
-    // Assertion Helpers
-    //==================================================================================================================
-
-    private static void assertRows(Iterable<String[]> rows) {
+        final var output = OUTPUT.toArray(String[][]::new);
         assert$(
-            Utils
-                .stream(rows)
-                .peek(row -> ROW_COUNTER.incrementAndGet())
-                .flatMap(Stream::of)
-                .noneMatch(String::isEmpty),
-            () -> "Unexpected rows: " + rows
+            equals(output, EXPECTED_OUTPUT),
+            () -> "Unexpected rows: expected=%s, given=%s".formatted(toString(EXPECTED_OUTPUT), toString(output))
         );
     }
 }
